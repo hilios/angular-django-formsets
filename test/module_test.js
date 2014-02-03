8c97faef2a0703ffd3cd838d9636296298e7d174
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-02-03 09:04:01 -0200

    Add module unit test

---
+++ test/module_test.js
@@ -0,0 +1,6 @@
+describe('djangoFormsets', function() {
+  it('should create the djangoFormsets module', function() {
+    var djangoFormsetsModule = angular.module('djangoFormsets');
+    expect(djangoFormsetsModule).to.be.defined;
+  });
+});
