b70f095e66095b04c0b8c94ca20f503bebc64498
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-02-03 08:51:41 -0200

    Add module modk to a global hook and some testing

--- test/config/global.js
+++ test/config/global.js
@@ -2,3 +2,5 @@ var chai = chai || require('chai'),
     assert = chai.assert,
     expect = chai.expect,
     should = chai.should();
+
+beforeEach(module('djangoFormsets'));
--- test/controllers_test.js
+++ test/controllers_test.js
@@ -1,6 +1,20 @@
 describe('djangoFormsetController', function(){
 
-  it('should test', function() {
-    expect(true).to.be.true;
+  var controller, attrs = {
+    djangoFormset: 'formset_template.html',
+    djangoFormsetPrefix: 'foo',
+    djangoFormsetCanDelete: true,
+    djangoFormsetCanOrder: true
+  };
+
+  beforeEach(inject(function($controller, $templateCache) {
+    $templateCache.put(attrs.djangoFormset, 
+      '<div django-formset-child>Template</di>');
+
+    controller = $controller('djangoFormsetController', {$attrs: attrs});
+  }));
+
+  it('should define a controller', function() {
+    expect(controller).to.be.defined;
   });
 });
